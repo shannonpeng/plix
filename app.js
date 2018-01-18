1a39b6b0cd1836624bd5d02d8afb6bafbb59e42d
shannonpeng <pengs@mit.edu>
2018-01-17 19:49:12 -0500

    Import mongoose

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
