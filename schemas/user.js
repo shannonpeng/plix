be88fa2a79dcc38e5595d22f71771800a6ce8745
shannonpeng <pengs@mit.edu>
2018-01-17 20:06:07 -0500

    Change from Jade to Handlebars

--- schemas/user.js
+++ schemas/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
     username: {type: String, required: true},
-    name: {type: String, required: true},
+    //name: {type: String, required: true},
     email: {type: String, required: true},
     hometown: {type: String, required: true},
     pixels: {type: Number, required: true},
