9767e43fa03d113bdf6115683cad72acb54d48c7
shannonpeng <pengs@mit.edu>
2018-01-18 22:12:59 -0500

    Update user schema

--- schemas/user.js
+++ schemas/user.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
-	username: {type: String, required: true},
-    email: {type: String, required: true},
-    hometown: {type: String, required: true},
+	username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
     pixels: {type: Number, required: true},
     last_pixel_at: {type: Number},
     boards: [String]
